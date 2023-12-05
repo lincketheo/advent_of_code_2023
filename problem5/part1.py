214770b41c5d16c1d4ac2e26e65cadbd6ef4c325
LinckeTheo <tjl@rincon.com>
2023-12-05 17:56:31 -0500

    initial pass

--- problem5/part1.py
+++ problem5/part1.py
@@ -1,45 +1,38 @@
 
 state = "unknown"
 
-seed_to_soil = 
+m = []
 
 with open("./sample.txt") as fp:
     for _line in fp.readlines():
-        if "seed-to-soil" in _line:
-            state = "seed-to-soil"
-            continue
-        if "soil-to-fertilizer" in _line:
-            state = "soil-to-fertilizer"
-            continue
-        if "fertilizer-to-water" in _line:
-            state = "fertilizer-to-water"
-            continue
-        if "water-to-light" in _line:
-            state = "water-to-light"
-            continue
-        if "light-to-temperature" in _line:
-            state = "light-to-temperature"
-            continue
-        if "temperature-to-humidity" in _line:
-            state = "temperature-to-humidity"
-            continue
-        if "humidity-to-location" in _line:
-            state = "humidity-to-location"
-            continue
+        data = _line.strip().split(" ")
+        if len(data) > 0 and data[0].isnumeric():
+            data = [int(x) for x in data]
 
-        line = _line.strip()
-        if len(line) == 0 or state == "unknown":
-            continue
+            dest = data[0]
+            source = data[1]
+            dist = data[2]
 
-        if state == "seed-to-soil":
-            arr = [int(x) for x in line.split(" ")]
+            # Dynamically size m
+            if source + dist > len(m):
+                if source > len(m):
+                    m += [i for i in range(source)]
 
-            dest = arr[0]
-            source = arr[1]
-            dist = arr[2]
+                for i in range(dist):
+                    if source + i > len(m):
+                        m += [source + i]
+
+            if dest + dist > len(m):
+                if source > len(m):
+                    m += [i for i in range(source)]
+
+                for i in range(dist):
+                    if dest + i > len(m):
+                        m += [dest + i]
             
-            sources = [source + i for i in range(dist)]
-            dests = [dest + i for i in range(dist)]
-            print(sources, dests)
-            
+            print(source, dest, dist)
+            print(m)
+            m[source:source+dist] = m[dest:dest+dist]
+
+print(min(m))
 
